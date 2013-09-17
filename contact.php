f2f0dd0dcf83362f560e9e5210aa1c43dce79e84
Cat <catia@Williams-MacBook-Air.local>
2013-09-17 15:01:27 -0400

    todo list, headshot on resume, progress on contact

--- contact.php
+++ contact.php
@@ -1,36 +1,12 @@
-$field_name = $_POST['cf_name'];
-$field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
-
-$mail_to = 'test@test-mail.com';
-
-$subject = 'Message from a site visitor ' . $field_name;
-
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
-
-$headers = "From: $cf_email\r\n";
-$headers .= "Reply-To: $cf_email\r\n";
-
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-if ($mail_status) { ?>
-    <script language="javascript" type="text/javascript">
-    // Print a message
-    alert('Thank you for the message. We will contact you shortly.');
-    // Redirect to some page of the site. You can also specify full URL, e.g. http://template-help.com
-    window.location = 'contact_page.html';
-    </script>
-    <?php
-}
-
-else { ?>
-    <script language="javascript" type="text/javascript">
-    // Print a message
-    alert('Message failed. Please, send an email to gordon@template-help.com');
-    // Redirect to some page of the site. You can also specify full URL, e.g. http://template-help.com
-    window.location = 'contact_page.html';
-    </script>
-    <?php
-}?>
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $formcontent=" From: $name \n Phone: $phone \n Message: $message";
+    $recipient = "catia3045@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+    ?>
