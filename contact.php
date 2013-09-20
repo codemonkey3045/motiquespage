bc278e265f837043e1348838eb90297df0bfcbda
Cat <catia@Williams-MacBook-Air.local>
2013-09-20 17:26:20 -0400

    choreography tab in progress.

--- contact.php
+++ contact.php
@@ -9,4 +9,4 @@
     $mailheader = "From: $email \r\n";
     mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
     echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
-    ?>
+?>
